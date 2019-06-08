e1916eced50c3ce2ec296098aa16539d4e511498
G.Bourel <gbourel@gmail.com>
2019-06-08 13:34:38 +0300

    [TP] Test existence coordonnees

--- tp_photos.py
+++ tp_photos.py
@@ -2,5 +2,11 @@
 
 import exif
 
-# affiche la latitude de la photo
-print(exif.latitude("photos/photo0.jpg"))
+# latitude et longitude de la photo
+lat = exif.latitude("photos/photo2.jpg")
+lon = exif.longitude("photos/photo2.jpg")
+
+if lat == None or lon == None:
+  print("Pas de coordonnées géographiques pour cette photo.")
+else:
+	print(lat, lon)
