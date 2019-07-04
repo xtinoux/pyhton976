250566ed45bb18279edce070b19983d656b15e4e
eclave <etienne.clave@ac-mayotte.fr>
2019-07-04 15:05:45 +0300

    Petites modifications déroulé et tp

--- tp_photos.py
+++ tp_photos.py
@@ -1,39 +1,40 @@
 # Objectif : classer les photos qui sont sur Mayotte ou non
 
-import geolocalisation
+from  geolocalisation import *
 import folium
 
 def add_marker(my_map, name, location):
+  """
+
+  """
   folium.Marker(
     location=location,
     popup=f"<img src='{name}' alt='{name}' height=250px width=250px/>",
     icon=folium.Icon(icon='picture')
   ).add_to(my_map)
 
-# liste avec uniquement les photos situees a Mayotte
-liste_mayotte = []
 
-# pour les 3 photos
-for index in range(3):
-  photo = "photos/photo{}.jpg".format(index)
-  # latitude et longitude de la photo
-  lat = geolocalisation.latitude(photo)
-  lon = geolocalisation.longitude(photo)
+liste_photo = []
 
-  # affiche les coordonnees si elles existent
-  if lat == None or lon == None:
- 	  print(photo, "Pas de coordonnées géographiques pour cette photo.")
-  else:
-    print(photo, lat, lon)
-    # si les coordonnees correspondent a Mayotte
+for i in range(0,571):
+  photo = 'photos/photo{}.jpg'.format(i)
+  lat = latitude(photo)
+  lon = longitude(photo)
+
+  if lat == None:
+    print("Pas de coordonnées")
+  else :
+    print('Y a des coordonnées')
     if lat > -13.1 and lat < -12.5 and lon > 44.5 and lon < 45.5:
-      liste_mayotte.append((photo, [lat, lon]))
+      print("La photo a été prise à Mayotte")
+      liste_photo.append(photo)
+print(liste_photo)
 
-print("Nombre de photos à Mayotte :", len(liste_mayotte))
 
 m = folium.Map(location=[-12.83,45.15], zoom_start=11)
 
-for photo in liste_mayotte:
-  add_marker(m, photo[0], photo[1])
+for photo in liste_photo:
+  add_marker(m, photo, (latitude(photo),longitude(photo)))
 
 m.save('map.html')
+
